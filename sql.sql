9fb1a0697123b64c91b4bfa1c973450cc41e3f7a
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-04-21 13:18:54 +0200

    off-duty job sql

---
+++ sql.sql
@@ -0,0 +1,10 @@
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+  (1000, 'off-duty', 1, 'off-duty1', 'Off-Duty', 0, '', ''),
+  (1001, 'off-duty', 2, 'off-duty2', 'Off-Duty', 0, '', ''),
+  (1002, 'off-duty', 3, 'off-duty3', 'Off-Duty', 0, '', ''),
+  (1003, 'off-duty', 4, 'off-duty4', 'Off-Duty', 0, '', ''),
+  (1004, 'off-duty', 0, 'off-duty', 'Off-Duty', 0, '', '');
+
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+      ('off-duty', 'off-duty', 1);
